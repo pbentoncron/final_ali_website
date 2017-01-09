ba4f86a4e549a150ce9d64a16de06d2766779aa8
Philip Cron <pbentoncron@gmail.com>
2017-01-08 20:49:31 -0600

    Edited application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'rails_12factor'
-require 'dotenv-rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
